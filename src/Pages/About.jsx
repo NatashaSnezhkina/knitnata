731a4885bc63be6b2929b01f3b60d010b8ddb371
Nataliia Snezhkina <nata.snezhkina@gmail.com>
2025-01-14 13:53:29 +0100

    initial commit

---
+++ src/Pages/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <section id="about" className="about--section">
+      <div className="about--content--box">
+        <h1>nata knit</h1>
+      </div>
+    </section>
+  );
+}
